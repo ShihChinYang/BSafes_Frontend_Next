9bbfaf5e73a96aaaadcce4f9c58a3f2b88f8a362
MacBook Pro - SYang <macbookpro-syang@MacBooks-MacBook-Pro.local>
2025-05-16 16:16:26 +0800

    Refactored Page.

--- pages/pageDemo/[itemId].jsx
+++ pages/pageDemo/[itemId].jsx
@@ -1,144 +1,29 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
-import Button from 'react-bootstrap/Button'
-
-import BSafesStyle from '../../styles/BSafes.module.css'
-
-import Scripts from "../../components/scripts";
-import ContentPageLayout from '../../components/layouts/contentPageLayout';
-import PageItemWrapper from "../../components/pageItemWrapper";
-
-import TopControlPanel from "../../components/topControlPanel"
-import PagePanel from "../../components/pagePanel";
-import TurningPageControls from "../../components/turningPageControls";
+import SinglePageCommon from "../../components/singlePageCommon";
 import SampleItemNoticeModal from "../../components/sampleItemNoticeModal";
 
 import { setDemoMode } from "../../reduxStore/auth";
-import { setDemoWorkspace, setNavigationInSameContainer } from "../../reduxStore/containerSlice";
-import { setChangingPage } from "../../reduxStore/pageSlice";
-
-import { debugLog } from "../../lib/helper";
+import { setDemoWorkspace } from "../../reduxStore/containerSlice";
 import { setupDemo } from "../../lib/demoHelper";
-import { getCoverAndContentsLink, getAnotherItem } from "../../lib/bSafesCommonUI"
 
 let hideFunction = false;
-if((process.env.NEXT_PUBLIC_platform ==='iOS') || (process.env.NEXT_PUBLIC_platform ==='android')) {
+if ((process.env.NEXT_PUBLIC_platform === 'iOS') || (process.env.NEXT_PUBLIC_platform === 'android')) {
     hideFunction = (process.env.NEXT_PUBLIC_functions.indexOf('hide') !== -1);
 }
 
 export default function Page() {
-    const debugOn = false;
-    debugLog(debugOn, "Rendering item");
-
-    const router = useRouter();
     const dispatch = useDispatch();
 
-    const [endOfContainer, setEndOfContainer] = useState(false);
     const [showNotice, setShowNotice] = useState(false);
-
-    const changingPage = useSelector(state => state.page.changingPage);
-    const pageItemId = useSelector(state => state.page.id);
-    const container = useSelector(state => state.page.container);
-    const position = useSelector(state => state.page.position);
-
-
-    const handleCoverClicked = () => {
-        if (!container) return;
-        let newLink = getCoverAndContentsLink(container).converLink;
-        router.push(newLink);
-    }
-
-    const handleContentsClicked = () => {
-        if (!container) return;
-        let newLink = getCoverAndContentsLink(container).contentsLink;
-        router.push(newLink);
-    }
-
-    async function gotoAnotherItem(anotherItemNumber) {
-        debugLog(debugOn, `gotoAnotherItem ${changingPage} ${pageItemId} ${container} ${position}`);
-        if (changingPage || !(pageItemId || !container || !position)) return;
-        setChangingPage(true);
-        let anotherItemId, anotherItemLink = null;
-
-        const getAnotherItemLink = (itemId) => {
-            const itemType = itemId.split(':')[0];
-            let itemLink;
-            switch (itemType) {
-                case 'b':
-                    itemLink = '/box/' + itemId;
-                    break;
-                case 'f':
-                    itemLink = '/folder/' + itemId;
-                    break;
-                case 'p':
-                    itemLink = '/page/' + itemId;
-                    break;
-                default:
-            }
-            return itemLink;
-        }
-
-        switch (anotherItemNumber) {
-            case '-1':
-                try {
-                    anotherItemId = await getAnotherItem('getPreviousItem', container, position, dispatch);
-                    if (anotherItemId === 'EndOfContainer') {
-                        setEndOfContainer(true);
-                    } else {
-                        anotherItemLink = getAnotherItemLink(anotherItemId);
-                    }
-                } catch (error) {
-
-                }
-                break;
-            case '+1':
-                try {
-                    anotherItemId = await getAnotherItem('getNextItem', container, position, dispatch);
-                    if (anotherItemId === 'EndOfContainer') {
-                        setEndOfContainer(true);
-                    } else {
-                        anotherItemLink = getAnotherItemLink(anotherItemId);
-                    }
-                } catch (error) {
-
-                }
-                break;
-            default:
-        }
-        debugLog(debugOn, "setNavigationInSameContainer ...");
-
-        setNavigationInSameContainer(true);
-        if (anotherItemLink) {
-            router.push(anotherItemLink);
-        } else {
-            setChangingPage(false);
-        }
-    }
-
-    const gotoNextItem = () => {
-        debugLog(debugOn, "Next item ");
-        gotoAnotherItem('+1');
-    }
-
-    const gotoPreviousItem = () => {
-        debugLog(debugOn, "Previous item ");
-        gotoAnotherItem('-1');
-    }
-
-    const onHide = () => {
-        router.push('/safe');
-    }
-
-    debugLog(debugOn, "router.query.itemId: ", router.query.itemId);
 
     const handleCloseNotice = () => {
         setShowNotice(false);
     }
 
     useEffect(() => {
-        if(setupDemo()){
+        if (setupDemo()) {
             dispatch(setDemoMode(true));
             dispatch(setDemoWorkspace());
             setShowNotice(true);
@@ -146,25 +31,9 @@ export default function Page() {
     }, []);
 
     return (
-        <div className={BSafesStyle.pageBackground}>
-            <ContentPageLayout>
-                <PageItemWrapper itemId={router.query.itemId}>
-                    <br />
-                    {hideFunction &&
-                            <Button variant='warning' onClick={onHide} className='float-end' style={{textTransform:'uppercase', color:'black'}}>
-                                Hide This page
-                            </Button>    
-                    }
-                    {!hideFunction &&
-                        <TopControlPanel onCoverClicked={handleCoverClicked} onContentsClicked={handleContentsClicked} ></TopControlPanel>
-                    }
-                    <PagePanel/>
-                    <TurningPageControls onNextClicked={gotoNextItem} onPreviousClicked={gotoPreviousItem} showAlert={endOfContainer} alertClosed={() => setEndOfContainer(false)} />
-                </PageItemWrapper>
-            </ContentPageLayout>
-            <Scripts />
-            <SampleItemNoticeModal show={showNotice} handleClose={handleCloseNotice}/>
-        </div>
-
+        <>
+            <SinglePageCommon demo={true}/>
+            <SampleItemNoticeModal show={showNotice} handleClose={handleCloseNotice} />
+        </>
     )
 }
